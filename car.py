c1203cc2b51803b55b2c4641d55eea9cff94a28b
cwchen1102 <chingwen.tech@gmail.com>
2023-10-25 17:40:42 +0800

    create class car

--- car.py
+++ car.py
@@ -1,10 +1,8 @@
-from abc import ABC, abstractmethod
+from service import Service
 
-
-class Car(ABC):
-    def __init__(self, last_service_date):
-        self.last_service_date = last_service_date
-
-    @abstractmethod
+class Car(Service):
+    def __init__(self, engine, battery):
+        self.engine = engine
+        self.battery = battery
     def needs_service(self):
-        pass
+        return self.engine.needs_service() or self.battery.needs_service()
