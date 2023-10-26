24ee3e34012bae9bd74d2a285e425f172b3a87aa
cwchen1102 <chingwen.tech@gmail.com>
2023-10-26 11:12:04 +0800

    update car_factory

--- car_factory.py
+++ car_factory.py
@@ -5,7 +5,7 @@ from engine.capulet_engine import CapuletEngine
 from engine.sternman_engine import SternmanEngine
 from engine.willoughby_engine import WilloughbyEngine
 
-class CarList:
+class CarFactory:
     def create_calliope(current_date, last_service_date, current_mileage, last_service_mileage):
         engine = CapuletEngine(current_mileage, last_service_mileage)
         battery = SpindlerBattery(current_date, last_service_date)
