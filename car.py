be58c596efbf9b1da236334dfe8fa856c3547f22
cwchen1102 <chingwen.tech@gmail.com>
2023-10-26 19:09:19 +0800

    add feature tires

--- car.py
+++ car.py
@@ -1,8 +1,9 @@
 from service import Service
 
 class Car(Service):
-    def __init__(self, engine, battery):
+    def __init__(self, engine, battery, tires):
         self.engine = engine
         self.battery = battery
+        self.tires = tires
     def needs_service(self):
-        return self.engine.needs_service() or self.battery.needs_service()
+        return self.engine.needs_service() or self.battery.needs_service() or self.tires.needs_service()
--- car_factories.py
+++ car_factories.py
@@ -5,38 +5,45 @@ from battery.spindler_battery import SpindlerBattery
 from engine.capulet_engine import CapuletEngine
 from engine.sternman_engine import SternmanEngine
 from engine.willoughby_engine import WilloughbyEngine
+from tires.carrigan_tires import CarriganTires
+from tires.octoprime_tires import OctoprimeTires
 
 class calliopeFactory(CarFactory):
-    def __init__(self, current_date, last_service_date, current_mileage, last_service_mileage):
+    def __init__(self, current_date, last_service_date, current_mileage, last_service_mileage, tire_wear):
         self.engine = CapuletEngine(current_mileage, last_service_mileage)
         self.battery = SpindlerBattery(current_date, last_service_date)
+        self.tires = CarriganTires(tire_wear)
     def get_car(self):
-        return Car(self.engine, self.battery)
+        return Car(self.engine, self.battery, self.tires)
 
 class glissadeFactory(CarFactory):
-    def __init__(self, current_date, last_service_date, current_mileage, last_service_mileage):
+    def __init__(self, current_date, last_service_date, current_mileage, last_service_mileage, tire_wear):
         self.engine = WilloughbyEngine(current_mileage, last_service_mileage)
         self.battery = SpindlerBattery(current_date, last_service_date)
+        self.tires = OctoprimeTires(tire_wear)
     def get_car(self):
-        return Car(self.engine, self.battery)
+        return Car(self.engine, self.battery, self.tires)
 
 class palindromeFactory(CarFactory):
-    def __init__(self, current_date, last_service_date, warning_light_is_on):
+    def __init__(self, current_date, last_service_date, warning_light_is_on, tire_wear):
         self.engine = SternmanEngine(warning_light_is_on)
         self.battery = SpindlerBattery(current_date, last_service_date)
+        self.tires = CarriganTires(tire_wear)
     def get_car(self):
-        return Car(self.engine, self.battery)
+        return Car(self.engine, self.battery, self.tires)
 
 class rorschachFactory(CarFactory):
-    def __init__(self, current_date, last_service_date, current_mileage, last_service_mileage):
+    def __init__(self, current_date, last_service_date, current_mileage, last_service_mileage, tire_wear):
         self.engine = WilloughbyEngine(current_mileage, last_service_mileage)
         self.battery = NubbinBattery(current_date, last_service_date)
+        self.tires = OctoprimeTires(tire_wear)        
     def get_car(self):
-        return Car(self.engine, self.battery)
+        return Car(self.engine, self.battery, self.tires)
 
 class thovexFactory(CarFactory):
-    def __init__(self, current_date, last_service_date, current_mileage, last_service_mileage):
+    def __init__(self, current_date, last_service_date, current_mileage, last_service_mileage, tire_wear):
         self.engine = CapuletEngine(current_mileage, last_service_mileage)
         self.battery = NubbinBattery(current_date, last_service_date)
+        self.tires = CarriganTires(tire_wear)
     def get_car(self):
-        return Car(self.engine, self.battery)    
+        return Car(self.engine, self.battery, self.tires)
